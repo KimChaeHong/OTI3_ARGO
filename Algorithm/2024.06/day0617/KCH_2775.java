92db3fd6e9afab2dcd8a40e21ce29a98f50a2452
hoi <coghd28@naver.com>
2024-06-18 23:47:28 +0900

    add

--- Algorithm/2024.06/day0617/KCH_2775.java
+++ Algorithm/2024.06/day0617/KCH_2775.java
@@ -1,20 +1,36 @@
 package day0617;
 
-public class KCH_2775 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		int [][] arr = new int[15][15];
-		
-		for (int i = 0; i < 15; i++) {
-			arr[0][i] = i;
+public class KCH_2775 {
+	public static int[][] APT = new int[15][15];
+
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+
+			sb.append(APT[k][n]).append('\n');
 		}
-		
+		System.out.println(sb);
+	
+
+	}
+	public static void arr() {
+		for (int i = 0; i < 15; i++) {
+			APT[0][i] = i;
+		}
+
 		for (int i = 1; i < 15; i++) {
 			for (int j = 1; j < 15; j++) {
-				arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
 			}
 		}
-
 	}
 
 }

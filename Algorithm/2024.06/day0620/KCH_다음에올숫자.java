46a46be566dfa07105f0a29a1cc3fa45744405a2
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-06-21 09:45:48 +0900

    feat 등비등차
    이해가안된다.

--- Algorithm/2024.06/day0620/KCH_다음에올숫자.java
+++ Algorithm/2024.06/day0620/KCH_다음에올숫자.java
@@ -1,37 +1,16 @@
 package day0620;
 
 public class KCH_다음에올숫자 {
-
 	public int solution(int[] common) {
-//		boolean check = isArithmetic(common);
-//		int temp = 0 ;
-//		int answer = 0;
-//		if (true == check) {
-//			temp = common[1] - common[0];
-//			answer  = common[common.length-1] + temp;
-//		} else {
-//			temp = common[1]/common[0];
-//			answer  = common[common.length-1] * temp;
-//		}
-//		return answer;
-//	}
-//
-//	public boolean isArithmetic(int[] common) {
-//		if (common[1] - common[0] == common[2] - common[1]) {
-//			
-//			return true;
-//		}
-//		return true;
-//	}
-//	
 	int answer = 0;
-    
+    //등차인지 등비일지 체크
     if ((common[1]-common[0]) == (common[2]-common[1])){
+    	//등차수열
         answer = common[common.length-1]+(common[1]-common[0]);
     } else{
+    	//등비수열
         answer = common[common.length-1]*((common[1]/common[0]));
     }
-    
     return answer;
 }
 
